93a114116c52f624dba38f95a435a638c45c6542
Sebastiano Moro <70955634+SebastianoMoro@users.noreply.github.com>
2020-11-19 22:45:45 +0100

    Update the structure of the pipeline

--- utils.py
+++ utils.py
@@ -333,95 +333,7 @@ def predict_fn_ssd(graph, image_path, **kwargs):
 
   return output_matrix
 
-def predict_fn_ssd(graph, image_path, **kwargs):
-    '''
-    INPUT
-        graph: path to the trained model
-        img_path: path to the image
-        
-    OUTPUT
-        Returns the bounding boxes as a np.array. Each row is a bounding box, each column is
-        (x, y, w/2, h/2, class_id, confidence)
-        (x,y): center of the bounding box
-        (w,h): width and height of the bounding box
-        class_id: numerical id of the class
-    '''
 
-    from object_detection.utils import ops as utils_ops
-    from object_detection.utils import label_map_util
-    from object_detection.utils import visualization_utils as vis_util
-    
-    def load_image_into_numpy_array(image):
-        (im_width, im_height) = image.size
-        return np.array(image.getdata()).reshape(
-          (im_height, im_width, 3)).astype(np.uint8)
-    
-    image = PIL.open(image_path)
-    image_np = load_image_into_numpy_array(image)
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-
-    with graph.as_default():
-        with tf.Session() as sess:
-            # Get handles to input and output tensors
-            ops = tf.get_default_graph().get_operations()
-            all_tensor_names = {output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
-            for key in [
-                      'num_detections', 'detection_boxes', 'detection_scores',
-                      'detection_classes', 'detection_masks'
-                  ]:
-                tensor_name = key + ':0'
-                if tensor_name in all_tensor_names:
-                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
-            if 'detection_masks' in tensor_dict:
-                # The following processing is only for single image
-                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                detection_boxes = tf.slice(detection_boxes, [0, 0], [
-                                            real_num_detection, -1])
-                detection_masks = tf.slice(detection_masks, [0, 0, 0], [
-                                            real_num_detection, -1, -1])
-                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                      detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                detection_masks_reframed = tf.cast(
-                      tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                    # Follow the convention by adding back the batch dimension
-                tensor_dict['detection_masks'] = tf.expand_dims(
-                      detection_masks_reframed, 0)
-                
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
-            # Run inference
-            output_dict = sess.run(tensor_dict,
-                                    feed_dict={image_tensor: np.expand_dims(image, 0)})
-
-            # all outputs are float32 numpy arrays, so convert types as appropriate
-            output_dict['num_detections'] = int(
-                                  output_dict['num_detections'][0])
-            output_dict['detection_classes'] = output_dict[
-                                  'detection_classes'][0].astype(np.uint8)
-            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            output_dict['detection_scores'] = output_dict['detection_scores'][0]
-            if 'detection_masks' in output_dict:
-                output_dict['detection_masks'] = output_dict['detection_masks'][0]
-                             
-
-            #custom part
-            im_height, im_width, _ = image.shape
-            d_boxes = output_dict['detection_boxes']
-            y = (d_boxes[:,0] + d_boxes[:,2])/2*im_height
-            x = (d_boxes[:,1] + d_boxes[:,3])/2*im_width
-            half_w = (d_boxes[:,3] - d_boxes[:,1])/2*im_width
-            half_h = (d_boxes[:,2] - d_boxes[:,0])/2*im_height
-            det_scores = output_dict['detection_scores']
-            class_id = output_dict['detection_classes']
-
-            output_matrix = np.dstack((x,y,half_w,half_h, class_id,det_scores))
-
-  
-    return np.squeeze(output_matrix)
 
 def generate_tfrecord(csv_input, output_path, image_dir):
 
@@ -440,7 +352,6 @@ def generate_tfrecord(csv_input, output_path, image_dir):
         return 0
   
   def split(df, group):
-    print('split')
     data = namedtuple('data', ['filename', 'object'])
     gb = df.groupby(group)
     return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
